9b94093583b173e714f2e70c646e92f4d138be7d
benvds <ben@pomodoro.nl>
2014-08-31 22:31:32 +0200

    warp clone methods

--- src/query_builder.rb
+++ src/query_builder.rb
@@ -2,6 +2,7 @@ require 'bundler/setup'
 Bundler.require(:default, :development)
 
 require 'date'
+require 'delegate'
 
 module QueryBuilder
   DB_FILE = 'tmp/query_builder.db'
@@ -21,7 +22,7 @@ module QueryBuilder
 
   def self.print_result_set(result_set)
     puts ""
-    puts result_set.sql
+    puts result_set.sql.to_s
     puts ""
 
     result_set.each do |row|
@@ -51,32 +52,54 @@ module QueryBuilder
     ]
   end
 
-  def self.apply_dimension(dataset, table_name)
-    dataset.join(table_name, id: :league_id).
-      group(Sequel::SQL::QualifiedIdentifier.new(table_name, 'id'))
+  class ReportDataset < SimpleDelegator
+
+    # Dataset methods calling internal clone method
+    CLONE_METHODS = %w(
+      distinct except from from_self group_cube group_rollup intersect
+      invert join_table lateral limit lock_style offset or order
+      qualify returning select select_all server unbind unfiltered ungrouped
+      union unlimited with with_recursive with_sql compound_clone
+    )
+
+    # wrap clone methods with this class again because cloning breaks delegation
+    CLONE_METHODS.each do |method_name|
+      define_method(method_name) do |*args, &block|
+        self.class.new(__getobj__().send(method_name, *args, &block))
+      end
+    end
+
+    def apply_dimension(table_name)
+      self.class.new(join(table_name, id: :league_id).
+        group(Sequel::SQL::QualifiedIdentifier.new(table_name, 'id')))
+    end
+
   end
 
-  sports = DB[:picks].
-    select { |o| [
-      o.count(o.picks__id).as('total'),
-      o.sports__id.as('dimension_id'),
-      o.sports__name.as('dimension_name'),
-      metric('avg', 'odd'),
-      metric('sum', 'stake')
-    ]}.
-    join(:leagues, id: :league_id).
-    join(:sports, id: :sport_id).
-    group(:sports__id)
+  # sports = DB[:picks].
+  #   select { |o| [
+  #     o.count(o.picks__id).as('total'),
+  #     o.sports__id.as('dimension_id'),
+  #     o.sports__name.as('dimension_name'),
+  #     metric('avg', 'odd'),
+  #     metric('sum', 'stake')
+  #   ]}.
+  #   join(:leagues, id: :league_id).
+  #   join(:sports, id: :sport_id).
+  #   group(:sports__id)
 
   # print_result_set(sports)
 
 
-  leagues = DB[:picks].
+  leagues = ReportDataset.new(DB[:picks])
+
+  leagues = leagues.
     select { |o| [
       o.count(o.picks__id).as('total')
-    ] + dimension_columns('leagues') + metric_columns }
+    ] + dimension_columns('leagues') + metric_columns }.
+    apply_dimension('leagues')
 
-  print_result_set(apply_dimension(leagues, 'leagues'))
+  print_result_set(leagues)
 
   # params = {
   #   dimension: 'sports',
