cfbedcd2825bc56944813e23496f5a5a865ca60d
benvds <ben@pomodoro.nl>
2014-08-31 14:03:22 +0200

    metric column helper

--- src/query_builder.rb
+++ src/query_builder.rb
@@ -19,23 +19,61 @@ module QueryBuilder
     QueryBuilder::SeedDB.run(DB)
   end
 
-  puts "picks count: #{ DB[:picks].count }"
+  def self.print_result_set(result_set)
+    puts ""
+    puts result_set.sql
+    puts ""
 
-  rs = DB[:picks].
-    select {[ count(:picks__id), avg(:picks__odd), :sports__name ]}.
+    result_set.each do |row|
+      puts row.inspect
+    end
+
+    puts ""
+  end
+
+  def self.qualified_column(column_name)
+    table = 'picks'
+    Sequel::SQL::QualifiedIdentifier.new(table, column_name)
+  end
+
+  def self.metric(function_name, column_name)
+    Sequel::SQL::Function.new(function_name, qualified_column(column_name)).
+      as("#{function_name}_#{column_name}")
+  end
+
+  sports = DB[:picks].
+    select { |o| [
+      o.count(o.picks__id).as('total'),
+      o.sports__id.as('dimension_id'),
+      o.sports__name.as('dimension_name'),
+      metric('avg', 'odd'),
+      metric('sum', 'stake')
+    ]}.
     join(:leagues, id: :league_id).
     join(:sports, id: :sport_id).
     group(:sports__id)
 
-  puts rs.sql
+  print_result_set(sports)
 
-  rs.each do |row|
-    puts row.inspect
-  end
+  leagues = DB[:picks].
+    select { |o| [
+      o.count(o.picks__id).as('total'),
+      o.leagues__id.as('dimension_id'),
+      o.leagues__name.as('dimension_name'),
+      metric('avg', 'odd'),
+      metric('sum', 'stake')
+    ]}.
+    join(:leagues, id: :league_id).
+    group(:leagues__id)
+
+  print_result_set(leagues)
 
   # params = {
-  #   dimension: :sports,
-  #   metrics: [:, :odd, :stake]
+  #   dimension: 'sports',
+  #   metrics: [
+  #     { agg: 'avg', column: 'odd' },
+  #     { agg: 'sum', column: 'stake' }
+  #   ]
   # }
 
 end
